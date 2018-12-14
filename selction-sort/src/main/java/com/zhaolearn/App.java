0b1ae5c14987374ddd2feed0566f9bac5b5c5190
RyzeZhao <643080112@qq.com>
2018-12-14 16:54:50 +0800

    选择排序

--- selction-sort/src/main/java/com/zhaolearn/App.java
+++ selction-sort/src/main/java/com/zhaolearn/App.java
@@ -1,13 +1,32 @@
 package com.zhaolearn;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void select_sort(int[] array){
+        for(int i=0;i< array.length-1;i++){
+            int minIndex = i;
+            for(int j=i+1;j<array.length;j++){
+                if(array[j]<array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i){
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 10, 124, 112, 3, 75, 11};
+        System.out.println("------------------------");
+        select_sort(arr);
     }
 }
