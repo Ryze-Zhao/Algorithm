707127a5cac8a71cc6a1910ba7d06db42901e740
RyzeZhao <643080112@qq.com>
2018-12-16 19:43:45 +0800

    选择排序

--- selction-sort/src/main/java/com/zhaolearn/SelectionSort.java
+++ selction-sort/src/main/java/com/zhaolearn/SelectionSort.java
@@ -4,34 +4,52 @@ import java.util.Arrays;
 
 /**
  * Hello world!
- *
  */
 public class SelectionSort {
-    public static void select_sort(int[] arr){
-        System.out.println("预计次数:"+arr.length*(arr.length-1)/2+" 原数组："+Arrays.toString(arr));
-        int forNum=0;//记录循环次数，不属于排序算法
-        for(int i=0;i< arr.length-1;i++){
+    public static void select_sort(int[] arr) {
+        System.out.println("预计次数:" + arr.length * (arr.length - 1) / 2 + " 原数组：" + Arrays.toString(arr));
+        int forNum = 0;//记录循环次数，不属于排序算法
+        for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[minIndex]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
-                System.out.println("---每次排序：" + Arrays.toString(arr));
+                //System.out.println("---每次排序：" + Arrays.toString(arr));
                 forNum++;
             }
-            if(minIndex != i){
+            if (minIndex != i) {
                 int temp = arr[i];
                 arr[i] = arr[minIndex];
                 arr[minIndex] = temp;
             }
             System.out.println("---第 " + (i + 1) + " 趟排序：" + Arrays.toString(arr));
         }
-        System.out.println("循环次数:"+forNum+" 排序后数组："+Arrays.toString(arr));
+        System.out.println("循环次数:" + forNum + " 排序后数组：" + Arrays.toString(arr));
+    }
+
+    public static void select_sort2(int[] arr) {
+        int temp;
+        for(int i=0;i<arr.length-1;i++){
+            int minInt=i;
+            for(int j=(i+1);j<arr.length;j++){
+                if(arr[minInt]>arr[j]){
+                    minInt=j;
+                }
+            }
+            if(arr[minInt]!=arr[i]){
+                temp = arr[i];
+                arr[i] = arr[minInt];
+                arr[minInt] = temp;
+            }
+        }
+        System.out.println( Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 10, 124, 112, 3, 75, 11};
         System.out.println("------------------------");
         select_sort(arr);
+//        select_sort2(arr);
     }
 }
