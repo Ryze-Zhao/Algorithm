d144a2888edc156a70dd96ebd9cbe81897f765ab
RyzeZhao <643080112@qq.com>
2018-12-28 20:20:06 +0800

    归并排序，更改，未完成

--- merge-sort/src/main/java/com/zhaolearn/MergeSort.java
+++ merge-sort/src/main/java/com/zhaolearn/MergeSort.java
@@ -5,29 +5,49 @@ package com.zhaolearn;
  *
  */
 public class MergeSort {
+    public static void merge_sort(int a[],int first,int last,int temp[]){
 
-    //将有序数组a[]和b[]合并到c[]中
-    public void MemeryArray(int a[], int n, int b[], int m, int c[])
-    {
-        int i, j, k;
-
-        i = j = k = 0;
-        while (i < n && j < m) {
-            if (a[i] < b[j]) {
-                c[k++] = a[i++];
-            } else {
-                c[k++] = b[j++];
-            }
-        }
-
-        while (i < n) {
-            c[k++] = a[i++];
-        }
-        while (j < m) {
-            c[k++] = b[j++];
+        if(first < last){
+            int middle = (first + last)/2;
+            merge_sort(a,first,middle,temp);//左半部分排好序
+            merge_sort(a,middle+1,last,temp);//右半部分排好序
+            mergeArray(a,first,middle,last,temp); //合并左右部分
         }
     }
 
+    //合并 ：将两个序列a[first-middle],a[middle+1-end]合并
+    public static void mergeArray(int a[],int first,int middle,int end,int temp[]){
+        int i = first;
+        int m = middle;
+        int j = middle+1;
+        int n = end;
+        int k = 0;
+        while(i<=m && j<=n){
+            if(a[i] <= a[j]){
+                temp[k] = a[i];
+                k++;
+                i++;
+            }else{
+                temp[k] = a[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<=m){
+            temp[k] = a[i];
+            k++;
+            i++;
+        }
+        while(j<=n){
+            temp[k] = a[j];
+            k++;
+            j++;
+        }
+
+        for(int ii=0;ii<k;ii++){
+            a[first + ii] = temp[ii];
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr = {21, 35, 10, 124, 112, 3, 75, 11};
