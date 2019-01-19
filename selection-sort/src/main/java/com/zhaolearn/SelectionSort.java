5ea1923d1701aac35dd2f589bc52cc688b2fa079
RyzeZhao <643080112@qq.com>
2019-01-19 18:06:21 +0800

    SelectionSort:选择排序

--- selection-sort/src/main/java/com/zhaolearn/SelectionSort.java
+++ selection-sort/src/main/java/com/zhaolearn/SelectionSort.java
@@ -1,55 +1,42 @@
 package com.zhaolearn;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 /**
  * Hello world!
  */
 public class SelectionSort {
     public static void select_sort(int[] arr) {
-        System.out.println("预计次数:" + arr.length * (arr.length - 1) / 2 + " 原数组：" + Arrays.toString(arr));
-        int forNum = 0;//记录循环次数，不属于排序算法
+        //用于记录次数用的数字
+        int num=0;
+        System.out.println("原数组："+Arrays.toString(arr));
+        //temp用于交换
+        int temp = 0;
         for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
+            //记录当前下标，暂时因为当前下标的对应arr[]值最小
+            int minIntIndex = i;
+            for (int j = arr.length - 1; i < j; j--) {
+                //一直寻找最小的，如果有就替换
+                if (arr[minIntIndex] > arr[j]) {
+                    minIntIndex = j;
                 }
-                //System.out.println("---每次排序：" + Arrays.toString(arr));
-                forNum++;
+                num++;
             }
-            if (minIndex != i) {
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
-            }
-            System.out.println("---第 " + (i + 1) + " 趟排序：" + Arrays.toString(arr));
-        }
-        System.out.println("循环次数:" + forNum + " 排序后数组：" + Arrays.toString(arr));
-    }
-
-    public static void select_sort2(int[] arr) {
-        int temp;
-        for(int i=0;i<arr.length-1;i++){
-            int minInt=i;
-            for(int j=(i+1);j<arr.length;j++){
-                if(arr[minInt]>arr[j]){
-                    minInt=j;
-                }
-            }
-            if(arr[minInt]!=arr[i]){
+            //如果默认值被改变，证明找到更小的数值，那就交换
+            if (minIntIndex != i) {
                 temp = arr[i];
-                arr[i] = arr[minInt];
-                arr[minInt] = temp;
+                arr[i] = arr[minIntIndex];
+                arr[minIntIndex] = temp;
             }
-        }
-        System.out.println( Arrays.toString(arr));
-    }
+            System.out.println("第"+(i+1)+"大次"+" 第"+num+"小次"+Arrays.toString(arr));
 
+        }
+        System.out.println("处理"+num+"次后数组："+Arrays.toString(arr));
+    }
     public static void main(String[] args) {
         int[] arr = {2, 3, 10, 124, 112, 3, 75, 11};
         System.out.println("------------------------");
         select_sort(arr);
-//        select_sort2(arr);
     }
 }
