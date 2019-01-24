db09b435077ef371699286559a64c87a6dad0be8
RyzeZhao <643080112@qq.com>
2019-01-24 22:10:50 +0800

    希尔排序(Shell Sort)：方法一

--- shell-sort/src/main/java/com/zhaolearn/ShellSort.java
+++ shell-sort/src/main/java/com/zhaolearn/ShellSort.java
@@ -8,39 +8,52 @@ import java.util.Arrays;
  */
 public class ShellSort {
     public static void shell_sort(int arr[]){
-        System.out.println("预计次数≤"+arr.length*(arr.length-1)/2+" 原数组："+Arrays.toString(arr));
+        int whileNum=0;
+System.out.println("预计次数≤"+arr.length*(arr.length-1)/2+" 原数组："+Arrays.toString(arr));
         int forNum=0;//记录循环次数，不属于排序算法
         int incre = arr.length;
         int temp = 0;
         while(true){
+            whileNum++;
             incre = incre/2;//数组分半
             for(int k = 0;k<incre;k++){    //根据增量分为若干子序列
                 for(int i=k+incre;i<arr.length;i+=incre){
                     for(int j=i;j>k;j-=incre){
+System.out.println("第"+whileNum+"轮while，其中incre为"+incre+",其中k为"+k+",其中arr.length为"+arr.length+",其中i为"+i+",其中j为"+j);
                         forNum++;
                         if(arr[j]<arr[j-incre]){
+System.out.println("  *****转换原因：arr["+(j-incre)+"](数值："+arr[j-incre]+")比arr["+j+"](数值："+arr[j]+")大,所以交换" );
                             temp = arr[j-incre];
                             arr[j-incre] = arr[j];
                             arr[j] = temp;
-                            System.out.println("---每次排序：" + Arrays.toString(arr));
+ //转换
+System.out.println("  ----转：" + Arrays.toString(arr));
                         }else{
-                            System.out.println("---每次排序：" + Arrays.toString(arr));
+System.out.println("  ----不：" + Arrays.toString(arr));
                             break;
                         }
                     }
-                    System.out.println("---第 " + (i + 1) + " 趟i排序：" + Arrays.toString(arr));
                 }
-                System.out.println("---第 " + (k + 1) + " 趟k排序：" + Arrays.toString(arr));
             }
             if(incre == 1){
                 break;
             }
-
         }
-        System.out.println("循环次数:"+forNum+" 排序后数组："+Arrays.toString(arr));
+System.out.println("循环次数:"+forNum+" 排序后数组："+Arrays.toString(arr));
     }
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        int[] arr = {21, 35, 10, 124, 112, 3, 75, 11};
+        int[] arr = {21, 35, 10, 124, 112, 3, 75, 11,15};
         System.out.println("------------------------");
         shell_sort(arr);
     }
