35c917b62e2073be43f550d97c611269d50d6fef
RyzeZhao <643080112@qq.com>
2019-01-27 13:29:45 +0800

    快速排序(QuickSort)：初始

--- quick-sort/src/main/java/com/zhaolearn/QuickSort.java
+++ quick-sort/src/main/java/com/zhaolearn/QuickSort.java
@@ -8,21 +8,16 @@ public class QuickSort{
     public static void quickSort(int a[],int l,int r){
         if(l>=r)
             return;
-
         int i = l; int j = r; int key = a[l];//选择第一个数为key
-
         while(i<j){
-
             while(i<j && a[j]>=key)//从右向左找第一个小于key的值
                 j--;
             if(i<j){
                 a[i] = a[j];
                 i++;
             }
-
             while(i<j && a[i]<key)//从左向右找第一个大于key的值
                 i++;
-
             if(i<j){
                 a[j] = a[i];
                 j--;
@@ -32,5 +27,10 @@ public class QuickSort{
         a[i] = key;
         quickSort(a, l, i-1);//递归调用
         quickSort(a, i+1, r);//递归调用
+    }
+    public static void main(String[] args) {
+        int[] arr = {21, 35, 10, 124, 112, 3, 75, 11};
+        System.out.println("------------------------");
+        quickSort(arr,1,1);
     }
 }
