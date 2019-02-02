20bbfa8fa4feba9fe4438a21362c9c927f7325c2
RyzeZhao <643080112@qq.com>
2019-02-02 16:25:07 +0800

    归并排序(Merge Sort):初始

--- merge-sort/src/main/java/com/zhaolearn/MergeSort.java
+++ merge-sort/src/main/java/com/zhaolearn/MergeSort.java
@@ -9,41 +9,54 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {21, 35, 10, 124, 112, 3, 75, 11};
-        int []temp = new int[arr.length];
         System.out.println("------------------------");
-        merge_sort(arr,0,7,temp);
-        System.out.println(Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length-1, new int[arr.length]);
+        System.out.println("排序完成："+Arrays.toString(arr));
     }
 
-    private static void merge_sort(int[] arr,int left,int right,int []temp){
-        if(left<right){
-            int mid = (left+right)/2;
-            merge_sort(arr,left,mid,temp);//左边归并排序，使得左子序列有序
-            merge_sort(arr,mid+1,right,temp);//右边归并排序，使得右子序列有序
-            merge(arr,left,mid,right,temp);//将两个有序子数组合并操作
+    public static void mergeSort(int a[], int first, int last, int temp[]) {
+        if (first < last) {
+            int middle = (first + last) / 2;
+            mergeSort(a, first, middle, temp);//左半部分排好序
+            mergeSort(a, middle + 1, last, temp);//右半部分排好序
+            mergeArray(a, first, middle, last, temp); //合并左右部分
+
         }
     }
-    private static void merge(int[] arr,int left,int mid,int right,int[] temp){
-        int i = left;//左序列指针
-        int j = mid+1;//右序列指针
-        int t = 0;//临时数组指针
-        while (i<=mid && j<=right){
-            if(arr[i]<=arr[j]){
-                temp[t++] = arr[i++];
-            }else {
-                temp[t++] = arr[j++];
+
+    public static void mergeArray(int a[], int first, int middle, int end, int temp[]) {
+        System.out.println("进入时：Arr："+Arrays.toString(a)+"  Temp:"+Arrays.toString(temp));
+        int i = first;
+        int m = middle;
+        int j = middle + 1;
+        int n = end;
+        int k = 0;
+        while (i <= m && j <= n) {
+            if (a[i] <= a[j]) {
+                temp[k] = a[i];
+                k++;
+                i++;
+            } else {
+                temp[k] = a[j];
+                k++;
+                j++;
             }
         }
-        while(i<=mid){//将左边剩余元素填充进temp中
-            temp[t++] = arr[i++];
+        while (i <= m) {
+            temp[k] = a[i];
+            k++;
+            i++;
         }
-        while(j<=right){//将右序列剩余元素填充进temp中
-            temp[t++] = arr[j++];
+        while (j <= n) {
+            temp[k] = a[j];
+            k++;
+            j++;
         }
-        t = 0;
-        //将temp中的元素全部拷贝到原数组中
-        while(left <= right){
-            arr[left++] = temp[t++];
+
+        for (int ii = 0; ii < k; ii++) {
+            a[first + ii] = temp[ii];
         }
+        System.out.println("结束时：Arr："+Arrays.toString(a)+"  Temp:"+Arrays.toString(temp));
+        System.out.println();
     }
 }
