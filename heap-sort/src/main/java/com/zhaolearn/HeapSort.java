19c643e0404ad3ee44d182286df48f268207180d
RyzeZhao <643080112@qq.com>
2019-02-18 21:30:19 +0800

    堆排序(HeapSort)：初始

--- heap-sort/src/main/java/com/zhaolearn/HeapSort.java
+++ heap-sort/src/main/java/com/zhaolearn/HeapSort.java
@@ -3,29 +3,30 @@ package com.zhaolearn;
 import java.util.Arrays;
 
 public class HeapSort {
-    public static void MinHeap_Sort(int a[],int n){
+    public static void MaxHeap_Sort(int arr[]){
         int temp = 0;
-        MakeMinHeap(a,n);
-        for(int i=n-1;i>0;i--){
-            temp = a[0];
-            a[0] = a[i];
-            a[i] = temp;
-            MinHeapFixdown(a,0,i);
+        //这一步将所有父节点进行交换
+        for(int i=(arr.length-1)/2 ; i>=0 ; i--){
+            System.out.println("父节点前："+Arrays.toString(arr)+" i的值："+i);
+            MaxHeapFixdown(arr,i,arr.length);
+            System.out.println("父节点后："+Arrays.toString(arr)+" i的值："+i);
+            System.out.println("--------------------------");
         }
-    }
-    //构建最小堆
-    public static void MakeMinHeap(int a[], int n){
-        for(int i=(n-1)/2 ; i>=0 ; i--){
-            MinHeapFixdown(a,i,n);
+        for(int i=arr.length-1;i>0;i--){
+            temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            System.out.println("子节点前："+Arrays.toString(arr)+" i的值："+i);
+            MaxHeapFixdown(arr,0,i);
+            System.out.println("子节点后："+Arrays.toString(arr)+" i的值："+i);
+            System.out.println("--------------------------");
         }
     }
     //这是从i节点开始调整,n为节点总数 从0开始计算 i节点的子节点为 2*i+1, 2*i+2
-    public static void MinHeapFixdown(int a[],int i,int n){
-
-        int j = 2*i+1; //子节点
+    public static void MaxHeapFixdown(int a[],int i,int n){
         int temp = 0;
+        for(int j=2*i+1;j<n;i=j,j=2*i+1){
 
-        while(j<n){
             //在左右子节点中寻找最小的
             if(j+1<n && a[j+1]<a[j]){
                 j++;
@@ -38,15 +39,13 @@ public class HeapSort {
             a[i] = a[j];
             a[j] = temp;
 
-            i = j;
-            j = 2*i+1;
         }
     }
 
     public static void main(String[] args) {
         int[] arr = {21, 35, 10, 124, 112, 3, 75, 11};
         System.out.println("--------------------------");
-        MinHeap_Sort(arr,arr.length);
+        MaxHeap_Sort(arr);
         System.out.println("排序完成："+Arrays.toString(arr));
     }
 }
